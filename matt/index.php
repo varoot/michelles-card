9f3047e154a33104452f7d9fca48f650cccb20d1
Varoot Phasuthadol <varoot@gmail.com>
2013-09-12 16:38:46 -0400

    Fix typo (I forgot "../")

--- matt/index.php
+++ matt/index.php
@@ -22,7 +22,7 @@
 	<!-- Javascript below -->
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="../js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
-	<script src="js/plugins.js"></script>
+	<script src="../js/plugins.js"></script>
 	<script>
 		$(function() {
 			var topDoc = $(top.document);
