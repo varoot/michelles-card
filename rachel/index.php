17c2859c07819be8b3ad1668bb52fdfd95a965ac
wongmatt <wongmatt@67-194-21-145.wireless.umnet.umich.edu>
2013-09-13 15:54:34 -0400

    made rachels font smaller

--- rachel/index.php
+++ rachel/index.php
@@ -10,17 +10,17 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="../css/normalize.css">
-	<link rel="style"
+	<link rel="stylesheet" type="text/css" href="../rachel/generic.css">
 	<link rel="stylesheet/less" type="text/css" href="style.less">
 	<script src="../js/vendor/modernizr-2.6.2.min.js"></script>
 	<script src="../js/vendor/less-1.4.2.min.js"></script>
 </head>
 <body>
 	<div>
-<!-- 	Hi Michelle!! I cannot believe you are leaving me with a bunch of yucky boys :( !!<br>
-	It is okay, though, I wish you the absolute best in Korea and that God will work in so many ways in your life.<Br>
-	It was wonderful having you as a part of the team and I appreciate your cuteness, kindness, and unique laugh so much!<br>
-	May God bless you and keep you in all things to come. Take care of yourself and please send us updates from time to time! -->
+	Hi Michelle!! I cannot believe you are leaving me with a bunch of yucky boys :( !!<br><br>
+	It is okay, though, I wish you the absolute best in Korea and that God will work in so many ways in your life.<br><br>
+	It was wonderful having you as a part of the team and I appreciate your cuteness, kindness, and unique laugh so much!<br><br>
+	May God bless you and keep you in all things to come. Take care of yourself and please send us updates from time to time!
 	</div>
 
 	<!-- Javascript below -->
