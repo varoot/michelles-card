41aec8909f5d5d5c645f0773c7aa02f9968d041b
Kevin <thekchang@gmail.com>
2013-09-13 16:03:50 -0400

    kevin chang

--- kevchang/index.php
+++ kevchang/index.php
@@ -18,6 +18,7 @@
 	<div>
 		<!-- Your message here -->
 		Dear Michelle,<br/>
+		Thank you so much for your dedication to web team.  I haven't been on it for all that long, but it really has been a blessing to serve on web team with you~ I am encouraged to see you following God's call to return to Korea, regardless of the potential obstacles.  At the same time, having to 'send you off' saddens me~  
 		
 	</div>
 
