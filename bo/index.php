58e226570f3f477133060d69962f32db344bd223
bjzhu <bojoezhu@gmail.com>
2013-09-28 18:11:52 -0400

    scrollbar added

--- bo/index.php
+++ bo/index.php
@@ -15,7 +15,7 @@
 	<script src="../js/vendor/less-1.4.2.min.js"></script>
 </head>
 <body>
-	<div>
+	<div style="width:170px; height:170px; overflow:scroll;">
 		Hey Michelle, sorry this isn't that cool, but I just wanted to write a word of encouragement to you as you transition back to Korea. It's been awesome serving with you on this team and I really think God's used you a lot with your gifts to bless the team and the church. I think it's awesome to see you able to use your design skills (I really enjoyed working on the Easter project with you), and I think you have an awesome gift in that area and how you get to continue to use it to build up the church and bless others. Another thing I'm encouraged by is how even though writing code isn't your forte, you've still worked through front-end code and been positive about learning it. I think you were an really awesome example to the team and I'm so glad you were part of it. We will definitely miss the female influence, the korean BBQ (which was so sdflajsdfa;dslf amazing), and the fun laughs. Praying for you as you go to Korea and excited to see what God will do and has in store for you there! - Bo
 	</div>
 
@@ -28,5 +28,7 @@
 			var topDoc = $(top.document);
 		})
 	</script>
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/slimbox2.js"></script>
 </body>
 </html>
